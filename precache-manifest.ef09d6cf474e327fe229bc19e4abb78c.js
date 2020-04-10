f77f96c67736778ac7ff9c087648e0eacfbb19a6
Tae Hee Choi <tchoi@nitinol.internal>
2020-04-10 16:18:45 -0700

    Updates

---
+++ precache-manifest.ef09d6cf474e327fe229bc19e4abb78c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95e2954bbeb68ef001f9acc916587b1",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "0cec23d5bc6c2f0515d8",
+    "url": "/react-tutorial/static/css/main.9862b2cc.chunk.css"
+  },
+  {
+    "revision": "7ff70d0960700cd5790a",
+    "url": "/react-tutorial/static/js/2.9e4f50fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.9e4f50fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cec23d5bc6c2f0515d8",
+    "url": "/react-tutorial/static/js/main.2c0b5685.chunk.js"
+  },
+  {
+    "revision": "866126c9f9440fa1d2f3",
+    "url": "/react-tutorial/static/js/runtime-main.6d017b08.js"
+  }
+]);
